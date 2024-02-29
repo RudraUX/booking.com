4dbaf4f0afae78bc77703cbdb1cdc64d1f5f0842
RudraUX <pollabrock@gmail.com>
2024-02-29 19:00:18 +0600

    register login added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose
   .connect(process.env.MONGODB_URL as string)
@@ -14,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-
 
 app.listen(7000, () => {
   console.log("Server is running on localhost:7000");
